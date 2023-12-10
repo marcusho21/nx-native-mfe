2a15dcac477a70a982c12823192bf0f9d1ef50c8
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2023-12-10 16:36:48 -0500

    add federation and resolve module can't resolve

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,6 +1,6 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation('/assets/federation.manifest.json')
+initFederation('./assets/federation.manifest.json')
   .catch((err) => console.error(err))
   .then(() => import('./bootstrap'))
   .catch((err) => console.error(err));
